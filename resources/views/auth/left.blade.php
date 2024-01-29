eef5f4caf3ffaab68f0c4cc0284956015ff12f59
Achmad Fawait <fawait626@gmail.com>
2024-01-29 21:15:25 +0700

    fix

---
+++ resources/views/auth/left.blade.php
@@ -0,0 +1,15 @@
+<div class="col-xl-4 col-lg-5 col-md-5 p-0 h-full">
+    <div class="signUP-admin-left signIn-admin-left position-relative">
+        <div class="signUP-overlay">
+            <img class="svg signupTop" src="{{ asset('assets/backend/img/svg/signuptop.svg') }}" alt="img" />
+            {{-- <img class="svg signupBottom" src="{{ asset('assets/backend/img/svg/car-rent.svg') }}" alt="img" /> --}}
+        </div><!-- End: .signUP-overlay  -->
+        <div class="signUP-admin-left__content">
+            <img src="{{ asset('logo.png') }}" class="img-fluid mb-4" width="60px" alt="">
+            <h1>EasyCarRent. Dashboard session</h1>
+        </div><!-- End: .signUP-admin-left__content  -->
+        <div class="signUP-admin-left__img">
+            <img class="img-fluid" src="{{ asset('') }}car-rent.svg" alt="img" width="800px" />
+        </div><!-- End: .signUP-admin-left__img  -->
+    </div><!-- End: .signUP-admin-left  -->
+</div><!-- End: .col-xl-4  -->
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -50,7 +50,7 @@
 
     <!-- endinject -->
 
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/backend/img/favicon.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('logo.png') }}">
 </head>
 
 <body>
@@ -59,37 +59,14 @@
         <div class="signUP-admin">
             <div class="container-fluid">
                 <div class="row justify-content-center">
-                    <div class="col-xl-4 col-lg-5 col-md-5 p-0">
-                        <div class="signUP-admin-left position-relative">
-                            <div class="signUP-overlay">
-                                <img class="svg signupTop" src="{{ asset('assets/backend/img/svg') }}/signuptop.svg"
-                                    alt="img" />
-                                <img class="svg signupBottom"
-                                    src="{{ asset('assets/backend/img/svg') }}/signupbottom.svg" alt="img" />
-                            </div>
-                            <div class="signUP-admin-left__content">
-                                <div
-                                    class="text-capitalize mb-md-30 mb-15 d-flex align-items-center justify-content-md-start justify-content-center">
-                                    <a class="wh-36 bg-primary text-white radius-md mr-10 content-center"
-                                        href="index.html">a</a>
-                                    <span class="text-dark">admin</span>
-                                </div>
-                                <h1>Bootstrap 4 React Web Application</h1>
-                            </div>
-                            <div class="signUP-admin-left__img">
-                                <img class="img-fluid svg"
-                                    src="{{ asset('assets/backend/img/svg') }}/signupIllustration.svg"
-                                    alt="img" />
-                            </div>
-                        </div>
-                    </div>
+                    @include('auth.left')
                     <div class="col-xl-8 col-lg-7 col-md-7 col-sm-8">
                         <div class="signUp-admin-right  p-md-40 p-10">
                             <div
                                 class="signUp-topbar d-flex align-items-center justify-content-md-end justify-content-center mt-md-0 mb-md-0 mt-20 mb-1">
                                 <p class="mb-0">
                                     Already have an account?
-                                    <a href="login.html" class="color-primary">
+                                    <a href="{{ route('login') }}" class="color-primary">
                                         Sign in
                                     </a>
                                 </p>
